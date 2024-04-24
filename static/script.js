ac7a783e27c8838bdcfda345e637efae220da86f
Amy <111530748+ameeSun@users.noreply.github.com>
2024-04-24 12:17:29 +0000

    final

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
-
+function getDate(){
+    const d = new Date();
+    document.getElementById("date").innerHTML = d;
+    // TBD display current date in the span tag with id="date" in web page footer
+    // hint: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_current
+}
+function toggleButton(){
+    if(document.getElementById("txtSearch").value == "")
+    {
+        document.getElementById("btnSearch").disabled = true;
+    }
+    else{
+        document.getElementById("btnSearch").disabled = false;
+    }
+}
